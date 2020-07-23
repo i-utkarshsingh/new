5a7d0157a94d878799d5179b1b63c0c964930a71
unknown <you@example.com>
2020-07-23 22:10:39 +0530

    Sign IN page added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import HomePage from './pages/homepage/homepage.component';
 import './App.css';
 import {Route,Switch} from 'react-router-dom';
 import ShopPage from  './pages/shop/shop.component.jsx'
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div >
+    <Header />
     <Switch>
        <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
+      <Route path='/signin' component={SignInAndSignUpPage} />
     </Switch>
      
     </div>
