0e7bf4cf510e6a43d7c74a1e40d40f85992356e5
unknown <you@example.com>
2020-08-07 15:53:32 +0530

    Added ways to pull data form Firebase and the REST version also.

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -5,39 +5,70 @@ import { connect } from "react-redux";
 import CollectionOverview from "../../components/collections-overview/collections-overview.components";
 import CollectionPage from "../collection/collection.component";
 
-import { firestore , convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
+
+import WithSpinner from "../../components/with-spinner/with-spinner.component";
 
 import { updateCollection } from "../../redux/shop/shop.actions";
 
+const CollectionOverviewWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
+  state = {
+    loading: true,
+  };
+
   unsubscribeFromSnapshot = null;
 
   componentDidMount() {
-    const {updateCollection} = this.props;
-    const collectionRef = firestore.collection('collection');
-    
-    collectionRef.onSnapshot(async snapshop => {
+    const { updateCollection } = this.props;
+    const collectionRef = firestore.collection("collection");
+
+    //The REST API method of getting data fro database but this will not be used as this is much slower as compared.
+    // fetch(
+    //   "https://firestore.googleapis.com/v1/projects/clothinng-bad/databases/(default)/documents/collection"
+    // )
+    //   .then(response => response.json())
+    //   .then(ans => console.log(ans))
+    //   .catch((err) => console.log(err));
+
+    collectionRef.get().then(async (snapshop) => {
       const collectionsMap = convertCollectionsSnapshotToMap(snapshop);
       updateCollection(collectionsMap);
-    })
+      this.setState({ loading: false });
+    });
   }
 
   render() {
     const { match } = this.props;
+    const { loading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          component={CollectionPage}
+          render={(props) => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
   }
 }
 
-const mapDispatchToProps = dispatch => ({
-  updateCollection : collectionMap => dispatch(updateCollection(collectionMap))
-})
+const mapDispatchToProps = (dispatch) => ({
+  updateCollection: (collectionMap) =>
+    dispatch(updateCollection(collectionMap)),
+});
 
-export default connect(null,mapDispatchToProps)(ShopPage);
+export default connect(null, mapDispatchToProps)(ShopPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -2,7 +2,6 @@ import { createSelector } from "reselect";
 
 const selectShop = (state) => state.shop;
 
-
 export const selectCollections = createSelector(
   [selectShop],
   (shop) => shop.collections
@@ -10,12 +9,11 @@ export const selectCollections = createSelector(
 
 export const selectCollectionForPreview = createSelector(
   [selectCollections],
-  collections => Object.keys(collections).map(key => collections[key])
-)
+  (collections) =>
+    collections ? Object.keys(collections).map((key) => collections[key]) : []
+);
 
-
-export const selectCollection = collectionUrlParam => 
-createSelector(
-  [selectCollections],
-  collections => collections[collectionUrlParam]
-)
+export const selectCollection = (collectionUrlParam) =>
+  createSelector([selectCollections], (collections) =>
+    collections ? collections[collectionUrlParam] : null
+  );
